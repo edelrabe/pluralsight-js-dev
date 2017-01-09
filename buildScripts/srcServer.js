30acd0b5866db84d8fe84cd11347dd24a8aa7b39
Salavat Yanberdin <edelrabe@gmail.com>
2017-01-09 17:19:01 +0100

    sync

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@test.com"},
+    {"id": 1, "firstName": "Alice", "lastName": "Cooper", "email": "alice@test.com"},
+    {"id": 1, "firstName": "Test", "lastName": "User", "email": "test@test.com"}
+  ]);
+});
+
 app.listen(port, function(err){
   if (err) {
     console.log(err);
--- src/index.test.js
+++ src/index.test.js
@@ -12,11 +12,11 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-  it('should say hello', (done) => {
+  it('should have h1 that says Users', (done) => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     jsdom.env(index, function(err, window){
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect("Hello!").to.equal(h1.innerHTML);
+      expect("Users").to.equal(h1.innerHTML);
       done();
       window.close();
     });

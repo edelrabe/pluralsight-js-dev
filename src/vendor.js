b71686d03fc444c3c35295a15ce779beed2861c5
Salavat Yanberdin <edelrabe@gmail.com>
2017-01-12 17:34:10 +0100

    sync

--- src/vendor.js
+++ src/vendor.js
@@ -1,2 +1,9 @@
+/**
+ * This file contains references to the vendor libs.
+ * This is used by webpack in prod build only.
+ * It's useful since this code won't change as often as the application's code.
+ *
+ */
+
 /* eslint-disable no-unused-vars */
 import fetch from 'whatwg-fetch';
